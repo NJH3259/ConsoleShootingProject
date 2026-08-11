09e7e0f85cdec8db1504356a4aa842b58f03926e
NamJongha <plazanimo@ajou.ac.kr>
2026-08-11 22:08:50 +0900

    게임 레벨 진행 중  및 에셋

--- Game/Level/GameLevel.cpp
+++ Game/Level/GameLevel.cpp
@@ -1,11 +1,29 @@
 #include "GameLevel.h"
-#include <Actor/Player.h>
+
+#include <cassert>
+#include <iostream>
 
 void GameLevel::OnInitialized()
 {
 	Level::OnInitialized();
+	
+	LoadMap("Stage.txt");
+}
 
-	//레벨에 테스트 액터 추가
-	SpawnActor<Player>();
-	//SpawnActor<Level>(); //정의에서 T타입으로 Actor의 하위 구문이어야 한다는 조건 때문에 SpawnActor<Level>은 호출 불가
+void GameLevel::Draw()
+{
+	Level::Draw();
+}
+
+void GameLevel::LoadMap(const std::string& fileName)
+{
+	std::string filePath = std::string("../Assets/") + fileName;
+	
+	FILE* file = nullptr;
+	fopen_s(&file, filePath.c_str(), "rt");
+	
+	if (!file)
+	{
+		assert(false && "Can't read Stage.txt file")
+	}
 }
