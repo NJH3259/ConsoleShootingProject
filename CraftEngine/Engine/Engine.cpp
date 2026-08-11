f8a24b7f26f3e4d15eb48ee49c9117aa2fbc0600
NamJongHa <plazanimo@gmail.com>
2026-08-11 10:23:02 +0900

    Start Project

---
+++ CraftEngine/Engine/Engine.cpp
@@ -0,0 +1,50 @@
+﻿#include "Engine.h"
+
+#include <iostream>
+#include <Windows.h>
+
+
+namespace Craft {
+	Engine::Engine()
+	{}
+	Engine::~Engine()
+	{}
+
+	void Engine::Run()
+	{
+		//프레임 레이트 구현
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+
+		LARGE_INTEGER counter;
+		QueryPerformanceCounter(&counter);
+
+		int64_t current = counter.QuadPart;
+		int64_t previous = current;
+
+		float oneFrameTime = 1.0f / setting.framerate;
+
+		//엔진 루프
+		while (true) {
+			if (isQuit) {
+				return;
+			}
+		}
+	}
+	void Engine::Quit()
+	{}
+	void Engine::OnInitilized()
+	{}
+	void Engine::ProcessInput()
+	{}
+	void Engine::BeginGamePlay()
+	{}
+	void Engine::Tick(float deltaTime)
+	{}
+	void Engine::Draw()
+	{}
+	void Engine::SavePreviousInputStates()
+	{}
+	void Engine::ShutDown()
+	{}
+}
---
+++ CraftEngine/Engine/Engine.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+
+namespace Craft {
+	class Engine
+	{
+		struct Setting {
+			float framerate;
+		};
+
+	public:
+		Engine();
+		virtual ~Engine();
+
+		void Run();
+
+		void Quit();
+
+	protected:
+		//엔진에서 해야할 일
+		//엔진 초기화
+		//입력 처리
+		//게임 플레이 초기화
+		//업데이트
+		//그리기
+		//프레임 간 입력 값 저장
+		//엔진 종료 시 정리 -> 필요하면 구현
+		void OnInitilized();
+
+		void ProcessInput();
+
+		void BeginGamePlay();
+
+		void Tick(float deltaTime);
+
+		void Draw();
+
+		void SavePreviousInputStates();
+
+		void ShutDown();
+
+	protected:
+		//엔진 종료 여부
+		bool isQuit = false;
+
+		//설정값
+		Setting setting;
+	};
+}
