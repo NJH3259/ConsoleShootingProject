4b732be2108a38ac815c984d7446735883253269
NamJongHa <plazanimo@gmail.com>
2026-08-12 17:51:44 +0900

    매 디버깅 랜덤 시드 생성

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,9 +1,11 @@
 #include <Engine/Engine.h>
 #include <Level/TestLevel.h>
+#include <Util/Util.h>
 
 int main()
 {
 	Craft::Engine engine;
+	Util::SetRandomSeed();
 	engine.AddNewLevel<TestLevel>();
 	engine.Run();
 }
